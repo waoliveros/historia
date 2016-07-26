e8ebec12f81087f85828d8189fd723c291b29684
Wigi Vei Oliveros <wvoliveros@olx.ph>
2016-07-26 18:20:28 +0800

    Use Swagger for publishing API

---
+++ config.sample.py
@@ -0,0 +1,18 @@
+LISTEN_IP = '0.0.0.0'
+LISTEN_PORT = 80
+
+# Swagger
+SWAGGER_HOST = ''
+SWAGGER_INFO = {
+    'title': 'Historia API',
+    'version': '0.1',
+    'description': 'Event Logger',
+    'contact': {
+        'name': 'Wigi Vei Oliveros',
+        'url': 'https://sayote.net',
+    },
+    'license': {
+        'name': 'MIT License',
+        'url': 'https://github.com/waoliveros/historia/blob/master/LICENSE',
+    },
+}
--- historia.py
+++ historia.py
@@ -1,6 +1,12 @@
 from eve import Eve
+from eve_swagger import swagger
+from config import *
 
 app = Eve()
+app.register_blueprint(swagger)
+
+app.config['SWAGGER_INFO'] = SWAGGER_INFO
+app.config['SWAGGER_HOST'] = SWAGGER_HOST
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host=LISTEN_IP, port=LISTEN_PORT)
